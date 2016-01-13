ad8babe024bdccad1d50776b07d4942b02a77f6a
Mihail Miklasevskij <mikla79@gmail.com>
2016-01-13 15:37:36 +0100

    Components, controller, service...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var app = angular.module('viserRss', []);
+app = angular.module('viser', [])
 
-app.factory('FeedService',function($http){
+// servis za pozivanje google API za obradu RSS
+.factory('FeedService', function($http){
     return {
         parseFeed : function(url){
             return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
@@ -8,4 +9,19 @@ app.factory('FeedService',function($http){
     };
 });
 
-app.controller("FeedCtrl", ['$scope','FeedService', FeedCtrl]);
+// app.controller('feedc', ['FeedService', function($scope, FeedService){
+app.controller('feedc',  function($scope, FeedService){
+  $scope.loadButonText = "Load"; //dugme tekst
+  console.log('hello');
+
+  $scope.controllerok = 'test text';
+  $scope.loadFeed = function(e){
+      FeedService.parseFeed($scope.feedSrc)
+      .then(function(res){
+          $scope.loadButonText = angular.element(e.target).text();
+          $scope.feeds = res.data.responseData.feed.entries;
+
+
+      });
+  };
+});
